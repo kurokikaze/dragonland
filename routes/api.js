c546f937ee22e94c0d8f2d4657099a96b657066f
Sergey Shirokov <kurokikaze@yandex.ru>
2020-02-17 14:07:17 +0000

    Moved server action conversion to separate file

--- routes/api.js
+++ routes/api.js
@@ -2,33 +2,20 @@ const express = require('express');
 const nanoid = require('nanoid');
 const moonlands = require('moonlands');
 const {constructDeck} = require('../utils');
+
 const convertClientCommand = require('../utils/convertClientCommand');
+const convertServerCommand = require('../utils/convertServerCommand');
 
 const {
-	ACTION_PASS,
 	ACTION_EFFECT,
-	ACTION_ENTER_PROMPT,
-
-	PROMPT_TYPE_CHOOSE_CARDS,
 
 	EFFECT_TYPE_CARD_MOVED_BETWEEN_ZONES,
-	EFFECT_TYPE_ADD_ENERGY_TO_MAGI,
-	EFFECT_TYPE_PAYING_ENERGY_FOR_CREATURE,
-	EFFECT_TYPE_ADD_ENERGY_TO_CREATURE,
-	EFFECT_TYPE_DISCARD_ENERGY_FROM_CREATURE,
-	EFFECT_TYPE_DISCARD_ENERGY_FROM_MAGI,
-	EFFECT_TYPE_PAYING_ENERGY_FOR_SPELL,
-	EFFECT_TYPE_MOVE_ENERGY,
 
-	ZONE_TYPE_DISCARD,
-	ZONE_TYPE_DECK,
 	ZONE_TYPE_IN_PLAY,
 } = require('moonlands/src/const');
 
 const ACTION_DISPLAY = 'display';
 const ZONE_UPDATE = 'subtypes/zone_update';
-
-const NUMBER_OF_STEPS = 6;
 
 var router = express.Router();
 
@@ -112,190 +99,36 @@ router.get(/^\/game\/([a-zA-Z0-9_-]+)\/(\d)$/, function(req, res) {
 			// Converting game actions for sending
 			runningGames[gameId].actionStreamOne.on('action', action => 
 			{
-				switch(action.type) {
-					case ACTION_PASS: {
-						const step = runningGames[gameId].state.step;
+				if (action.type === ACTION_EFFECT && action.effectType === EFFECT_TYPE_CARD_MOVED_BETWEEN_ZONES) {
+					const player = action.destinationCard.owner;
+					const sourceZoneContent = runningGames[gameId].getZone(
+						action.sourceZone, 
+						(action.sourceZone == ZONE_TYPE_IN_PLAY) ? null : player,
+					).serialize();
 
-						const newStep = (step === null) ? 0 : (step + 1) % NUMBER_OF_STEPS;
+					const destinationZoneContent = runningGames[gameId].getZone(
+						action.destinationZone,
+						(action.destinationZone == ZONE_TYPE_IN_PLAY) ? null : player,
+					).serialize();
 
-						action = {
-							...action,
-							newStep,
-						};
-						break;
-					}
-					case ACTION_ENTER_PROMPT: {
-						switch(action.promptType) {
-							case PROMPT_TYPE_CHOOSE_CARDS: {
-								const discardCards = runningGames[gameId].getZone(ZONE_TYPE_DISCARD, action.player).cards.map(card => card.card.name);
-								const libraryCards = runningGames[gameId].getZone(ZONE_TYPE_DECK, action.player).cards.map(card => card.card.name);
-								const searchableCards = [...discardCards, ...libraryCards];
-								const availableCards = action.promptParams.filter(card => searchableCards.includes(card));
+					socket.emit(ACTION_DISPLAY, {
+						subtype: ZONE_UPDATE,
+						zoneType: action.sourceZone,
+						player,
+						content: sourceZoneContent,
+					});
 
-								action.availableCards = availableCards;                                
-								break;
-							}
-						}
-						break;
-					}
-					case ACTION_EFFECT: {
-						switch(action.effectType) {
-							case EFFECT_TYPE_CARD_MOVED_BETWEEN_ZONES: {
-								const player = action.destinationCard.owner;
-								const sourceZoneContent = runningGames[gameId].getZone(
-									action.sourceZone, 
-									(action.sourceZone == ZONE_TYPE_IN_PLAY) ? null : player,
-								).serialize();
-
-								const destinationZoneContent = runningGames[gameId].getZone(
-									action.destinationZone,
-									(action.destinationZone == ZONE_TYPE_IN_PLAY) ? null : player,
-								).serialize();
-
-								socket.emit(ACTION_DISPLAY, {
-									subtype: ZONE_UPDATE,
-									zoneType: action.sourceZone,
-									player,
-									content: sourceZoneContent,
-								});
-
-								socket.emit(ACTION_DISPLAY, {
-									subtype: ZONE_UPDATE,
-									zoneType: action.destinationZone,
-									player,
-									content: destinationZoneContent,
-								});
-								break;
-							}
-							case EFFECT_TYPE_PAYING_ENERGY_FOR_CREATURE: {
-								const fromCard = (typeof action.from == 'string') ?
-									runningGames[gameId].getMetaValue(action.from, action.generatedBy) :
-									action.from;
-								const from = (fromCard.length) ? fromCard[0] : fromCard;
-								from.card = from.card.card;
-
-								action = {
-									...action,
-									from,
-								};
-								break;
-							}
-							case EFFECT_TYPE_PAYING_ENERGY_FOR_SPELL: {
-								const fromCard = (typeof action.from == 'string') ?
-									runningGames[gameId].getMetaValue(action.from, action.generatedBy) :
-									action.from;
-								const from = (fromCard.length) ? fromCard[0] : fromCard;
-								from.card = from.card.card;
-
-								action = {
-									...action,
-									from,
-								};
-								break;
-							}
-							case EFFECT_TYPE_DISCARD_ENERGY_FROM_MAGI: {
-								const targetCard = (typeof action.target == 'string') ?
-									runningGames[gameId].getMetaValue(action.target, action.generatedBy) :
-									action.target;
-
-								const amount = (typeof action.amount == 'string') ?
-									runningGames[gameId].getMetaValue(action.amount, action.generatedBy) :
-									action.amount;
-                                
-								const target = (targetCard.length) ? targetCard[0] : targetCard;
-								target.card = target.card.card;
-
-								action = {
-									...action,
-									target,
-									amount,
-								};
-								break;
-							}
-							case EFFECT_TYPE_MOVE_ENERGY: {
-								const targetCard = (typeof action.target == 'string') ?
-									runningGames[gameId].getMetaValue(action.target, action.generatedBy) :
-									action.target;
-
-								const target = (targetCard.length) ? targetCard[0] : targetCard;
-								target.card = target.card.card;
-
-								const sourceCard = (typeof action.source == 'string') ?
-									runningGames[gameId].getMetaValue(action.source, action.generatedBy) :
-									action.source;
-
-								const source = (sourceCard.length) ? sourceCard[0] : sourceCard;
-								source.card = source.card.card;
-
-								const amount = (typeof action.amount == 'string') ?
-									runningGames[gameId].getMetaValue(action.amount, action.generatedBy) :
-									action.amount;
-
-								action = {
-									...action,
-									target,
-									source,
-									amount,
-								};
-								break;
-							}
-							case EFFECT_TYPE_DISCARD_ENERGY_FROM_CREATURE: {
-								const targetCard = (typeof action.target == 'string') ?
-									runningGames[gameId].getMetaValue(action.target, action.generatedBy) :
-									action.target;
-
-								const amount = (typeof action.amount == 'string') ?
-									runningGames[gameId].getMetaValue(action.amount, action.generatedBy) :
-									action.amount;
-                                
-								const target = (targetCard.length) ? targetCard[0] : targetCard;
-								target.card = target.card.card;
-
-								action = {
-									...action,
-									target,
-									amount,
-								};
-								break;
-							}
-							case EFFECT_TYPE_ADD_ENERGY_TO_CREATURE: {
-								const targetCard = (typeof action.target == 'string') ?
-									runningGames[gameId].getMetaValue(action.target, action.generatedBy) :
-									action.target;
-                                
-								const target = (targetCard.length) ? targetCard[0] : targetCard;
-								target.card = target.card.card;
-
-								const amount = (typeof action.amount == 'string') ?
-									runningGames[gameId].getMetaValue(action.amount, action.generatedBy) :
-									action.amount;
-
-								action = {
-									...action,
-									target,
-									amount,
-								};
-								break;
-							}
-							case EFFECT_TYPE_ADD_ENERGY_TO_MAGI: {
-								const targetCard = (typeof action.target == 'string') ?
-									runningGames[gameId].getMetaValue(action.target, action.generatedBy) :
-									action.target;
-                                
-								const target = (targetCard.length) ? targetCard[0] : targetCard;
-								target.card = target.card.card;
-
-								action = {
-									...action,
-									target,
-								};
-								break;
-							}
-						}
-						break;
-					}
+					socket.emit(ACTION_DISPLAY, {
+						subtype: ZONE_UPDATE,
+						zoneType: action.destinationZone,
+						player,
+						content: destinationZoneContent,
+					});
+					socket.emit('action', action);
+				} else {
+					const convertedAction = convertServerCommand(action, runningGames[gameId]);
+					socket.emit('action', convertedAction);
 				}
-				socket.emit('action', action);
 			});
 
 			// Converting client actions for game engine			
---
+++ utils/convertServerCommand.js
@@ -0,0 +1,183 @@
+const {
+	ACTION_PASS,
+	ACTION_ENTER_PROMPT,
+	ACTION_EFFECT,
+
+	PROMPT_TYPE_CHOOSE_CARDS,
+	// PROMPT_TYPE_SINGLE_CREATURE_OR_MAGI,
+
+	EFFECT_TYPE_ADD_ENERGY_TO_MAGI,
+	EFFECT_TYPE_PAYING_ENERGY_FOR_CREATURE,
+	EFFECT_TYPE_PAYING_ENERGY_FOR_SPELL,
+	EFFECT_TYPE_DISCARD_ENERGY_FROM_MAGI,
+	EFFECT_TYPE_MOVE_ENERGY,
+	EFFECT_TYPE_DISCARD_ENERGY_FROM_CREATURE,
+	EFFECT_TYPE_ADD_ENERGY_TO_CREATURE,
+
+	ZONE_TYPE_DECK,
+	ZONE_TYPE_DISCARD,
+} = require('moonlands/src/const');
+
+const NUMBER_OF_STEPS = 6;
+
+function convertServerCommand(action, game) {
+	switch(action.type) {
+		case ACTION_PASS: {
+			const step = game.state.step;
+
+			const newStep = (step === null) ? 0 : (step + 1) % NUMBER_OF_STEPS;
+
+			action = {
+				...action,
+				newStep,
+			};
+			break;
+		}
+		case ACTION_ENTER_PROMPT: {
+			switch(action.promptType) {
+				case PROMPT_TYPE_CHOOSE_CARDS: {
+					const discardCards = game.getZone(ZONE_TYPE_DISCARD, action.player).cards.map(card => card.card.name);
+					const libraryCards = game.getZone(ZONE_TYPE_DECK, action.player).cards.map(card => card.card.name);
+					const searchableCards = [...discardCards, ...libraryCards];
+					const availableCards = action.promptParams.filter(card => searchableCards.includes(card));
+
+					action.availableCards = availableCards;                                
+					break;
+				}
+			}
+			break;
+		}
+		case ACTION_EFFECT: {
+			switch(action.effectType) {
+				case EFFECT_TYPE_PAYING_ENERGY_FOR_CREATURE: {
+					const fromCard = (typeof action.from == 'string') ?
+						game.getMetaValue(action.from, action.generatedBy) :
+						action.from;
+					const from = (fromCard.length) ? fromCard[0] : fromCard;
+					from.card = from.card.card;
+
+					action = {
+						...action,
+						from,
+					};
+					break;
+				}
+				case EFFECT_TYPE_PAYING_ENERGY_FOR_SPELL: {
+					const fromCard = (typeof action.from == 'string') ?
+						game.getMetaValue(action.from, action.generatedBy) :
+						action.from;
+					const from = (fromCard.length) ? fromCard[0] : fromCard;
+					from.card = from.card.card;
+
+					action = {
+						...action,
+						from,
+					};
+					break;
+				}
+				case EFFECT_TYPE_DISCARD_ENERGY_FROM_MAGI: {
+					const targetCard = (typeof action.target == 'string') ?
+						game.getMetaValue(action.target, action.generatedBy) :
+						action.target;
+
+					const amount = (typeof action.amount == 'string') ?
+						game.getMetaValue(action.amount, action.generatedBy) :
+						action.amount;
+                    
+					const target = (targetCard.length) ? targetCard[0] : targetCard;
+					target.card = target.card.card;
+
+					action = {
+						...action,
+						target,
+						amount,
+					};
+					break;
+				}
+				case EFFECT_TYPE_MOVE_ENERGY: {
+					const targetCard = (typeof action.target == 'string') ?
+						game.getMetaValue(action.target, action.generatedBy) :
+						action.target;
+
+					const target = (targetCard.length) ? targetCard[0] : targetCard;
+					target.card = target.card.card;
+
+					const sourceCard = (typeof action.source == 'string') ?
+						game.getMetaValue(action.source, action.generatedBy) :
+						action.source;
+
+					const source = (sourceCard.length) ? sourceCard[0] : sourceCard;
+					source.card = source.card.card;
+
+					const amount = (typeof action.amount == 'string') ?
+						game.getMetaValue(action.amount, action.generatedBy) :
+						action.amount;
+
+					action = {
+						...action,
+						target,
+						source,
+						amount,
+					};
+					break;
+				}
+				case EFFECT_TYPE_DISCARD_ENERGY_FROM_CREATURE: {
+					const targetCard = (typeof action.target == 'string') ?
+						game.getMetaValue(action.target, action.generatedBy) :
+						action.target;
+
+					const amount = (typeof action.amount == 'string') ?
+						game.getMetaValue(action.amount, action.generatedBy) :
+						action.amount;
+                    
+					const target = (targetCard.length) ? targetCard[0] : targetCard;
+					target.card = target.card.card;
+
+					action = {
+						...action,
+						target,
+						amount,
+					};
+					break;
+				}
+				case EFFECT_TYPE_ADD_ENERGY_TO_CREATURE: {
+					const targetCard = (typeof action.target == 'string') ?
+						game.getMetaValue(action.target, action.generatedBy) :
+						action.target;
+                    
+					const target = (targetCard.length) ? targetCard[0] : targetCard;
+					target.card = target.card.card;
+
+					const amount = (typeof action.amount == 'string') ?
+						game.getMetaValue(action.amount, action.generatedBy) :
+						action.amount;
+
+					action = {
+						...action,
+						target,
+						amount,
+					};
+					break;
+				}
+				case EFFECT_TYPE_ADD_ENERGY_TO_MAGI: {
+					const targetCard = (typeof action.target == 'string') ?
+						game.getMetaValue(action.target, action.generatedBy) :
+						action.target;
+                    
+					const target = (targetCard.length) ? targetCard[0] : targetCard;
+					target.card = target.card.card;
+
+					action = {
+						...action,
+						target,
+					};
+					break;
+				}
+			}
+			break;
+		}
+	}
+	return action;
+}
+
+module.exports = convertServerCommand;
